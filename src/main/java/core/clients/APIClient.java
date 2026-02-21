5bb339447807cf79e45c87012d8eed2e3557acea
bahojuk <bahojuk@gmail.com>
2026-02-21 18:07:13 +0300

    Написал тест на getbookoingById

--- src/main/java/core/clients/APIClient.java
+++ src/main/java/core/clients/APIClient.java
@@ -1,5 +1,10 @@
 package core.clients;
 
+import core.settings.ApiEndpoints;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -27,5 +32,44 @@ public class APIClient {
         }
 
         return properties.getProperty("baseUrl");
+    }
+
+    private RequestSpecification getRequestSpec() {
+        return RestAssured.given()
+                .baseUri(baseurl)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json");
+    }
+
+    //GET запрос на эндпоинт /ping
+    public Response ping() {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.PING.getPath()) // Используем ENUM для эндпоинта /ping
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+    }
+
+    //GET запрос на эндпоинт /booking
+    public Response getBooking() {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKING.getPath()) // Используем ENUM для эндпоинта /ping
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+    }
+
+    public Response getBookingById(int id) {
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKING.getPath() + "/" + id) // Используем ENUM для эндпоинта /ping
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
     }
 }
