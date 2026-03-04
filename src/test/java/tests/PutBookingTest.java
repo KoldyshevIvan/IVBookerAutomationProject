79ad6f334661eda597994d7ead23d1af5fa3ff90
bahojuk <bahojuk@gmail.com>
2026-03-04 12:57:09 +0300

    Добавил степы в PutBookingTest

--- src/test/java/tests/PutBookingTest.java
+++ src/test/java/tests/PutBookingTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,6 +24,9 @@ public class PutBookingTest {
     private CreatedBooking createdBooking;
     private NewBooking newBooking;
     private NewBooking updatedBooking;
+    private Response response;
+    private Response responseUpdatedBooking;
+    private NewBooking getUpdatedBooking;
 
     @BeforeEach
     public void setup() {
@@ -30,23 +34,27 @@ public class PutBookingTest {
         objectMapper = new ObjectMapper();
         apiClient.createToken("admin", "password123");
 
-        // Создаем объект Booking с необходимыми данными
-        newBooking = new NewBooking();
-        newBooking.setFirstname("Frosya");
-        newBooking.setLastname("Semenova");
-        newBooking.setTotalprice(444);
-        newBooking.setDepositpaid(true);
-        newBooking.setBookingdates(new NewBooking.Bookingdates("2020-01-01", "2025-02-02"));
-        newBooking.setAdditionalneeds("Breakfast");
+        step("Создать объект для нового бронирования newBooking", () -> {
+                    newBooking = new NewBooking();
+                    newBooking.setFirstname("Frosya");
+                    newBooking.setLastname("Semenova");
+                    newBooking.setTotalprice(444);
+                    newBooking.setDepositpaid(true);
+                    newBooking.setBookingdates(new NewBooking.Bookingdates("2020-01-01", "2025-02-02"));
+                    newBooking.setAdditionalneeds("Breakfast");
+                }
+        );
 
-        // Создаем объект updatedBooking, на который будем обновлять newBooking
-        updatedBooking = new NewBooking();
-        updatedBooking.setFirstname("Frosya");
-        updatedBooking.setLastname("Semenova");
-        updatedBooking.setTotalprice(555);
-        updatedBooking.setDepositpaid(false);
-        updatedBooking.setBookingdates(new NewBooking.Bookingdates("2021-01-01", "2025-02-02"));
-        updatedBooking.setAdditionalneeds("Breakfast");
+        step("Создать объект бронирования updatedBooking", () -> {
+                    updatedBooking = new NewBooking();
+                    updatedBooking.setFirstname("Frosya");
+                    updatedBooking.setLastname("Semenova");
+                    updatedBooking.setTotalprice(555);
+                    updatedBooking.setDepositpaid(false);
+                    updatedBooking.setBookingdates(new NewBooking.Bookingdates("2021-01-01", "2025-02-02"));
+                    updatedBooking.setAdditionalneeds("Breakfast");
+                }
+        );
     }
 
     @Test
@@ -55,49 +63,57 @@ public class PutBookingTest {
     @Owner("Ivan Koldyshev")
     public void testPutBooking() throws JsonProcessingException {
 
-        // Выполняем запрос на создание newBooking через APIClient
-        String requestBody = objectMapper.writeValueAsString(newBooking);
-        Response response = apiClient.createBooking(requestBody);
-        assertThat(response.getStatusCode()).isEqualTo(200);
+        step("Отправить JSON на энпоинт /booking и создать новое бронирование", () ->
+                {
+                    String requestBody = objectMapper.writeValueAsString(newBooking);
+                    response = apiClient.createBooking(requestBody);
+                    step("Проверить, что статус-код ответа == 200", () ->
+                            assertThat(response.getStatusCode()).isEqualTo(200)
+                    );
+                    String responseBody = response.asString();
+                    createdBooking = objectMapper.readValue(responseBody, CreatedBooking.class);
+                }
+        );
 
-        // Десериализуем тело ответа в объект newBooking
-        String responseBody = response.asString();
-        createdBooking = objectMapper.readValue(responseBody, CreatedBooking.class);
-
-        // Запоминаем id созданного бронирования
         int bookingId = createdBooking.getBookingid();
 
-        // Выполняем запрос на обновление newBooking на updatedBooking через APIClient
-        String requestUpdatedBookingBody = objectMapper.writeValueAsString(updatedBooking);
-        Response responseUpdatedBooking = apiClient.updateBooking(bookingId, requestUpdatedBookingBody);
-        assertThat(responseUpdatedBooking.getStatusCode()).isEqualTo(200);
+        step("Отправить JSON на энпоинт /booking на обновление бронирования", () ->
+                {
+                    String requestUpdatedBookingBody = objectMapper.writeValueAsString(updatedBooking);
+                    responseUpdatedBooking = apiClient.updateBooking(bookingId, requestUpdatedBookingBody);
+                    step("Проверить, что статус-код ответа == 200", () ->
+                            assertThat(responseUpdatedBooking.getStatusCode()).isEqualTo(200));
+                    Response responseGetUpdatedBooking = apiClient.getBookingById(bookingId);
+                    step("Проверить, что статус-код ответа == 200", () ->
+                            assertThat(responseGetUpdatedBooking.getStatusCode()).isEqualTo(200));
+                    String responseGetUpdatedBookingBody = responseGetUpdatedBooking.asString();
+                    getUpdatedBooking = objectMapper.readValue(responseGetUpdatedBookingBody, NewBooking.class);
+                }
+        );
 
-        // Получаем объект обновленного бронирования по id
-        Response responseGetUpdatedBooking = apiClient.getBookingById(bookingId);
-        assertThat(responseGetUpdatedBooking.getStatusCode()).isEqualTo(200);
-
-        // Десериализуем тело ответа в объект getUpdatedBooking
-        String responseGetUpdatedBookingBody = responseGetUpdatedBooking.asString();
-        NewBooking getUpdatedBooking = objectMapper.readValue(responseGetUpdatedBookingBody, NewBooking.class);
-
-        // Проверяем, что объект getUpdatedBooking, полученный по id, соответствует объекту updatedBooking, на который мы обновили объект newBooking
-        assertThat(getUpdatedBooking).isNotNull();
-        assertEquals(getUpdatedBooking.getFirstname(), updatedBooking.getFirstname());
-        assertEquals(getUpdatedBooking.getLastname(), updatedBooking.getLastname());
-        assertEquals(getUpdatedBooking.getTotalprice(), updatedBooking.getTotalprice());
-        assertEquals(getUpdatedBooking.getDepositpaid(), updatedBooking.getDepositpaid());
-        assertEquals(getUpdatedBooking.getBookingdates().getCheckin(), updatedBooking.getBookingdates().getCheckin());
-        assertEquals(getUpdatedBooking.getBookingdates().getCheckout(), updatedBooking.getBookingdates().getCheckout());
-        assertEquals(getUpdatedBooking.getAdditionalneeds(), updatedBooking.getAdditionalneeds());
+        step("Проверить, что все поля объекта getUpdatedBooking, полученного по id, соответствуют полям объекта updatedBooking, на который был обновлен объект newBooking", () -> {
+                    assertThat(getUpdatedBooking).isNotNull();
+                    assertEquals(getUpdatedBooking.getFirstname(), updatedBooking.getFirstname());
+                    assertEquals(getUpdatedBooking.getLastname(), updatedBooking.getLastname());
+                    assertEquals(getUpdatedBooking.getTotalprice(), updatedBooking.getTotalprice());
+                    assertEquals(getUpdatedBooking.getDepositpaid(), updatedBooking.getDepositpaid());
+                    assertEquals(getUpdatedBooking.getBookingdates().getCheckin(), updatedBooking.getBookingdates().getCheckin());
+                    assertEquals(getUpdatedBooking.getBookingdates().getCheckout(), updatedBooking.getBookingdates().getCheckout());
+                    assertEquals(getUpdatedBooking.getAdditionalneeds(), updatedBooking.getAdditionalneeds());
+                }
+        );
     }
 
     @AfterEach
     public void tearDown() {
-        // Удаляем созданное бронирование
-        apiClient.createToken("admin", "password123");
-        apiClient.deleteBooking(createdBooking.getBookingid());
 
-        // Проверяем, что бронирование успешно удалено
-        assertThat(apiClient.getBookingById(createdBooking.getBookingid()).getStatusCode()).isEqualTo(404);
+        step("Удалить созданное бронирование", () -> {
+                    apiClient.createToken("admin", "password123");
+                    apiClient.deleteBooking(createdBooking.getBookingid());
+
+                    step("Проверить, что статус-код ответа == 404", () ->
+                            assertThat(apiClient.getBookingById(createdBooking.getBookingid()).getStatusCode()).isEqualTo(404));
+                }
+        );
     }
 }
